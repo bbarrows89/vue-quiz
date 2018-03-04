6348ec790824e5229c4cb4f2832aa6eec624d7dc
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-03-03 19:51:30 -0800

    installed Axios, adding definitions

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,90 +1,20 @@
 <template>
   <div class="hello">
     <h2>Test your trivia knowledge with 10 questions from a chosen category.</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    
   </div>
 </template>
 
 <script>
+import {API} from '@/common/api';
+import CubeSpinner from '@/components/CubeSpinner';
+
+
 export default {
   name: 'Quiz',
+  components: {
+    'load-spinner': CubeSpinner
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
