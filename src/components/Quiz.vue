daf9c0e25118891882a1c59a5d56e95c73e11c1b
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2018-03-07 17:28:36 -0800

    toying with <select> bindings for categories

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -2,16 +2,23 @@
   <div class="quiz">
     <span>Test your trivia knowledge with</span>
     <select v-model="numQuestions">
-      <option disabled value="">Please select a number</option>
+      <option selected disabled value="">Please select a number</option>
       <option v-for="n in 20" :value="n" :key="n">{{n}}</option>
-      </select>
-    <span>questions from a chosen category.</span>
+    </select>
+    <select v-model="difficulty">
+      <option selected disabled value="">Select a difficulty</option>
+      <option>easy</option>
+      <option>medium</option>
+      <option>hard</option>
+    </select>
+    <span>questions from the following categories: </span>
     <load-spinner v-if="showLoading"></load-spinner>
     <select v-if="categories" v-model="currentCategory">
-      <option selected value="currentCategory">{{currentCategory.name}}</option>
-      <option v-for="category in categories" :value="category" :key="category.id">{{category.name}}</option>
+      <option selected disabled value="">categories.</option>
+      <option value="currentCategory">{{currentCategory.name}}</option>
+      <option v-for="category in categories" :value="category" :key="category.id">{{currentCategory.name}}</option>
     </select>
-    <p>You've chosen to answer {{numQuestions}} questions from the {{currentCategory.name}} category.</p>
+    <p>You've chosen to answer {{numQuestions}} {{difficulty}} difficulty questions from the: <br> {{currentCategory.name}} category.</p>
   </div>
 </template>
 
@@ -26,11 +33,12 @@ export default {
   },
   data () {
     return {
-      categories: [],
+      categories: null,
       currentCategory: { // chosen category (defaults to Random)
         name: 'Random',
         id: 9
       },
+      difficulty: '',
       questions: [], // current list of game questions
       numQuestions: 10,
       messages: [],
@@ -47,7 +55,7 @@ export default {
       this.showLoading = true
       API.get('https://opentdb.com/api_category.php')
         .then(response => {
-          this.$ls.set('categories', response.data)
+          this.$ls.set('categories', response.data.trivia_categories)
           console.log('Categories have been retrieved and cached.')
           this.categories = response.data.trivia_categories
           this.showLoading = false
@@ -62,7 +70,9 @@ export default {
     }
   },
   methods: {
+    getQuestions: function (event) {
 
+    }
   }
 }
 </script>
